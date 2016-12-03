ef5c70a3a1171445877bb8ff5d8df6137d7ef370
Adrian Wesolowski <rzalibul95@gmail.com>
2016-12-03 14:49:20 +0000

    Date formatting added for comments

--- init_website.py
+++ init_website.py
@@ -2,13 +2,14 @@ from flask import Flask, render_template
 from flask import request
 import csv
 import json
+from datetime import datetime
 
 # reads a csv file in specified path
 def readCsvFile(filePath):
 	with open(filePath, 'r') as inFile:
 		reader = csv.reader(inFile)
 		list = [row for row in reader]
-	return aList
+	return list
 # writes specified list to a csv file in specified path
 def writeCsvFile(list, filePath):
 	with open(filePath, 'w', newline='') as outFile:
@@ -42,12 +43,21 @@ def gallery():
 
 @app.route('/saveComment', methods=['POST'])
 def saveComment():
+	commentsPath = "static\\comments.csv"
 	name = request.form['author']
 	comment = request.form['comment']
-	rating = int(request.form['rating'])
+	rating = request.form['rating']
 	
+	# format result is present in the csv file
+	date = datetime.utcnow().strftime("%d %b %Y %H:%M:%S %Z %z")
+	# to do: proper timezone formatting (use pytz maybe)
+	
+	list = readCsvFile(commentsPath)
+	newEntry = [name, comment, rating, date]
+	list.append(newEntry)
+	writeCsvFile(list, commentsPath)
 	# dump data sent in JSON format for test purposes
-	return json.dumps({'status': 'OK', 'author': name, 'comment': comment, 'rating': rating})
+	return json.dumps({'status': 'OK', 'author': name, 'comment': comment, 'rating': rating, 'date': date})
 
 if __name__ == '__main__':
 	app.run(debug = True)
