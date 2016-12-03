c03e1b7e78699cf846082241666cb61f878d20db
Adrian Wesolowski <rzalibul95@gmail.com>
2016-12-03 12:23:44 +0000

    Read/write CSV file functions added

--- init_website.py
+++ init_website.py
@@ -2,8 +2,22 @@ from flask import Flask, render_template
 from flask import request
 import csv
 
+# reads a csv file in specified path
+def readCsvFile(filePath):
+	with open(filePath, 'r') as inFile:
+		reader = csv.reader(inFile)
+		list = [row for row in reader]
+	return aList
+# writes specified list to a csv file in specified path
+def writeCsvFile(list, filePath):
+	with open(filePath, 'w', newline='') as outFile:
+		writer = csv.writer(outFile)
+		writer.writerows(list)
+	return
+
 app = Flask(__name__)
 
+# double routing so the standard path is index page
 @app.route('/')
 @app.route('/index')
 def home():
